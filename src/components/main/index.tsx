962db3266a1f5efc46e34818b08f10d031878dad
Naszh <naszh99@gmail.com>
2023-02-20 11:28:11 +0300

    add main without timer

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,34 @@
+import {
+  Wrapper,
+  TextHeader,
+  HeaderBottom,
+  Paragraph,
+  Container,
+  TextButton,
+  Button,
+} from './styled';
+
 export const Main = () => {
   return (
-    <>
-      <></>
-    </>
+    <Wrapper>
+      <TextHeader>Under Construction</TextHeader>
+      <HeaderBottom>
+        We're making lots of improvements and will be back soon
+      </HeaderBottom>
+      <Container>
+        <Paragraph>Check our event page when you wait:</Paragraph>
+        <Button>
+          <TextButton>
+            <a
+              href='https://www.linkedin.com/in/naszh/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Go to the event
+            </a>
+          </TextButton>
+        </Button>
+      </Container>
+    </Wrapper>
   );
 };
--- src/components/main/styled.ts
+++ src/components/main/styled.ts
@@ -1,1 +1,54 @@
 import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+  margin: 0 auto;
+  text-align: center;
+`;
+
+export const TextHeader = styled.h1`
+  margin-bottom: 8px;
+  font-size: 72px;
+  font-weight: 700;
+  line-height: 104%;
+  text-transform: uppercase;
+  color: #162c4e;
+  opacity: 39%;
+`;
+
+export const Paragraph = styled.p`
+  margin: auto;
+`;
+
+export const HeaderBottom = styled(Paragraph)`
+  max-width: 423px;
+  margin-bottom: 32px;
+`;
+
+export const Container = styled.section`
+  margin: 72px auto 76px;
+`;
+
+export const Button = styled.button`
+  position: relative;
+  margin-top: 12px;
+  width: 240px;
+  height: 60px;
+  padding: 16px 41px 17px 28px;
+  outline: none;
+  border: none;
+  border-radius: 40px;
+  background: #1d4169;
+  cursor: pointer;
+  &::after {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    content: url('../../../imgs/arrow-right.png');
+    height: 20px;
+  }
+`;
+
+export const TextButton = styled(Paragraph)`
+  text-align: left;
+  color: #ffffff;
+`;
